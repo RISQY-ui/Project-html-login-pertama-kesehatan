02365c0d91e6ce351d71ac648510d0794c800253
RISQY-ui <rizkifaris703@gmail.com>
2026-03-15 14:43:01 +0700

    Create belajar js.js

---
+++ belajar js.js
@@ -0,0 +1,13 @@
+function infoAir(){
+alert("Air membantu tubuh tetap sehat dan mencegah dehidrasi.");
+}
+
+function infoOlahraga(){
+alert("Olahraga membantu jantung lebih sehat dan tubuh lebih kuat.");
+}
+
+function ubahWarna(){
+document.body.style.background = "#e8f8f5";
+document.getElementById("judul").innerHTML = "Tetap Sehat Hari Ini!";
+}
+
