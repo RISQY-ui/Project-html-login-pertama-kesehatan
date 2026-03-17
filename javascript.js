a659125956d34ef815482af5b8a0fc9f13487b39
RISQY-ui <rizkifaris703@gmail.com>
2026-03-18 05:48:22 +0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 function klik(){
-alert("SUDAH LOGIN BELUM");
+  alert("SUDAH LOGIN BELUM");
 }
+
 function sarip() {
   alert("LOGIN UNTUK CEK BMI KAMU");
+  console.log('yaho');
+  prompt("sorry yeh");
 }
